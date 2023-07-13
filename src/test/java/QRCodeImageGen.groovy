060e8d1dc44a5ec1f24eef31a8ee646c9bc9f484
koala114 <32124993+koala114@users.noreply.github.com>
2023-07-13 15:38:48 +0800

    add coupon cancel and refund

--- src/test/java/QRCodeImageGen.groovy
+++ src/test/java/QRCodeImageGen.groovy
@@ -1,3 +1,4 @@
+import com.alibaba.fastjson.JSON
 import com.kargo.internal.GenerateQRCode
 import com.kargo.request.QRCodeRequest
 import com.kargo.response.QRCodeRequestResponse
@@ -10,6 +11,10 @@ class QRCodeImageGen extends Helper {
         qrCodeRequest.setOrder_no("2030400122177501")
         qrCodeRequest.setPay_amt(2.50)
         qrCodeRequest.setTimestamp("1688711768156")
+        when:
         QRCodeRequestResponse qrCodeRequestResponse = GenerateQRCode.generate(qrCodeRequest, "LawsonPoshub@2018");
+        def result = JSON.toJSON(qrCodeRequestResponse)
+        then:
+        result.ret_code == '0000'
     }
 }
